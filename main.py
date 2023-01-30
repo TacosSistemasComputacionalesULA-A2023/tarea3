721fab89561baf0dbe9b331e47e4b73bb0c5f5aa
Jorge Luis Rodríguez González <egroj97@gmail.com>
2023-01-30 15:20:34 -0400

    Added automation of testing, and comments

--- main.py
+++ main.py
@@ -6,118 +6,164 @@ import time
 from agent import ValueIteration
 import numpy as np
 import csv
+import multiprocessing
 
 # Allowing environment to have sounds
 if "SDL_AUDIODRIVER" in os.environ:
     del os.environ["SDL_AUDIODRIVER"]
 
+fieldnames = [
+    "iter_num",
+    "method",
+    "gamma",
+    "delta",
+    "states_values",
+    "policies_values",
+    "reward",
+]
 
+
+class agent_arguments:
+    """
+    agent_arguments allows to encapsulate all of the parameters needed to run a
+    new episode for our agent and doing concurrently on the system that is
+    running it.
+    """
+
+    def __init__(self, env, experiments, iterations, method, init_values, gamma, delta):
+        self.experiments = experiments
+        self.iterations = iterations
+        self.method = method
+        self.init_values = init_values
+        self.gamma = gamma
+        self.delta = delta
+        self.env = env
+
+    def __str__(self) -> str:
+        return f"<{self.iterations}|{self.method}|{self.gamma}|{self.delta}>"
+
+
+# sums allow us to get the sum of all the policies obtained in each experiment.
+# So that later we can calculate the average.
 def sums(policies, values, averages):
     for i in range(len(policies)):
-        average_values["policies_values"][i] += policies[i]
+        averages["policies_values"][i] += policies[i]
 
     for i in range(len(values)):
-        average_values["states_values"][i] += values[i]
+        averages["states_values"][i] += values[i]
+
+
+# run learning process takes the arguments for the agent and runs the specified
+# amount of experiments also while returning summaries from that experiments set.
+def run_learning_process(arguments: agent_arguments):
+    space_num = arguments.env.observation_space.n
+
+    average_values = {
+        "states_values": [0 for _ in range(space_num)],
+        "policies_values": [0 for _ in range(space_num)],
+        "reward": 0,
+    }
+
+    for _ in range(arguments.experiments):
+        agent = ValueIteration(
+            states_n=space_num,
+            actions_n=arguments.env.action_space.n,
+            P=arguments.env.P,
+            gamma=arguments.gamma,
+        )
+
+        agent.reset(arguments.init_values[0], arguments.init_values[1])
+
+        agent.solve(
+            policy_evaluations=arguments.iterations,
+            iterations=arguments.iterations,
+            delta=arguments.delta,
+            method=arguments.method,
+        )
+
+        # agent.render()
+        observation, _ = arguments.env.reset()
+        terminated, truncated = False, False
+
+        arguments.env.render()
+
+        reward = 0
+        while not (terminated or truncated):
+            action = agent.get_action(observation)
+            (
+                observation,
+                reward,
+                terminated,
+                truncated,
+                _,
+            ) = arguments.env.step(action)
+
+        average_values["reward"] += reward
+        sums(agent.policy, agent.values, average_values)
+        arguments.env.close()
+    for i in range(len(average_values["policies_values"])):
+        average_values["policies_values"][i] /= arguments.experiments
+
+    for i in range(len(average_values["states_values"])):
+        average_values["states_values"][i] /= arguments.experiments
+
+    average_values["reward"] /= arguments.experiments
+
+    average_values["method"] = arguments.method
+
+    average_values["iter_num"] = arguments.iterations
+
+    average_values["gamma"] = arguments.gamma
+
+    average_values["delta"] = arguments.delta
+
+    return average_values
 
 
 if __name__ == "__main__":
+
     env = gym.make("FrozenLake-v0", render_mode="ansi", delay=0)
-    space_num = env.observation_space.n
 
     experiments = 100
 
-    iterations = [100, 1000, 10000, 100000]
-    evaluations = [100, 1000, 10000, 100000]
+    iterations = [100, 1000, 10000]
     methods = ["valiter", "politer"]
+    space_num = env.observation_space.n
     init_values = [
         (np.zeros(space_num), (np.zeros(space_num))),
         (np.random.rand(space_num), np.random.randint(0, 4, space_num)),
-        ([1 for _ in range(space_num)], [1 for _ in range(space_num)]),
-    ]
-    fieldnames = [
-        "iter_num",
-        "eval_num",
-        "method",
-        "gamma",
-        "delta",
-        "states_values",
-        "policies_values",
-        "reward",
     ]
 
-    f = open("summary.csv", "a")
+    f = open("summary.csv", "a", newline="")
     with f:
         writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writeheader()
 
+    arguments = []
+
     for method in methods:
-        for evaluation in evaluations:
-            for iteration in iterations:
-                for gamma in np.arange(0.1, 1.0, 0.1):
-                    for delta in np.arange(0.1, 1.0, 0.1):
-                        for init_value in init_values:
-                            average_values = {
-                                "states_values": [0 for _ in range(space_num)],
-                                "policies_values": [0 for _ in range(space_num)],
-                                "reward": 0,
-                            }
+        for iteration in iterations:
+            for gamma in np.arange(0.1, 1.0, 0.1):
+                for delta in np.arange(0.1, 1.0, 0.1):
+                    for init_value in init_values:
+                        arguments.append(
+                            agent_arguments(
+                                iterations=iteration,
+                                init_values=init_value,
+                                gamma=gamma,
+                                delta=delta,
+                                method=method,
+                                env=env,
+                                experiments=experiments,
+                            )
+                        )
 
-                            for _ in range(experiments):
-                                agent = ValueIteration(
-                                    states_n=space_num,
-                                    actions_n=env.action_space.n,
-                                    P=env.P,
-                                    gamma=gamma,
-                                )
+    p = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+    result = p.map_async(run_learning_process, arguments)
+    values = result.get()
 
-                                agent.reset(init_value[0], init_value[1])
+    f = open("summary.csv", "a", newline="")
+    with f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
 
-                                agent.solve(
-                                    policy_evaluations=evaluation,
-                                    iterations=iteration,
-                                    delta=delta,
-                                    method=methods,
-                                )
-
-                                # agent.render()
-                                observation, info = env.reset()
-                                terminated, truncated = False, False
-
-                                env.render()
-
-                                while not (terminated or truncated):
-                                    action = agent.get_action(observation)
-                                    (
-                                        observation,
-                                        reward,
-                                        terminated,
-                                        truncated,
-                                        _,
-                                    ) = env.step(action)
-
-                                average_values["reward"] += reward
-                                sums(agent.policy, agent.values, average_values)
-                                env.close()
-
-                            f = open("summary.csv", "a")
-                            with f:
-                                writer = csv.DictWriter(f, fieldnames=fieldnames)
-                                for i in range(len(average_values["policies_values"])):
-                                    average_values["policies_values"][i] /= experiments
-
-                                for i in range(len(average_values["states_values"])):
-                                    average_values["states_values"][i] /= experiments
-
-                                average_values["reward"] /= experiments
-
-                                average_values["method"] = method
-
-                                average_values["iter_num"] = iteration
-
-                                average_values["eval_num"] = evaluation
-                                
-                                average_values["gamma"] = gamma
-                                
-                                average_values["delta"] = delta
-
-                                writer.writerow(average_values)
+        writer.writerows(values)
